2221d12d55184d19535c877a2a5e0fa5cdcf874e
costanzazanoletti <zanoletti.cos@gmail.com>
2022-07-20 13:10:28 +0200

    add controller and view for book form

--- src/main/java/jana60/controller/BookController.java
+++ src/main/java/jana60/controller/BookController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import jana60.model.Book;
 import jana60.repository.BookRepository;
 
 @Controller
@@ -18,5 +19,15 @@ public class BookController {
   public String bookList(Model model) {
     model.addAttribute("books", repo.findAll());
     return "/book/list";
+  }
+
+  /*
+   * controller che ritorna la view con la form
+   * vuota per aggiungere un nuovo book
+   */
+  @GetMapping("/add")
+  public String bookForm(Model model) {
+    model.addAttribute("book", new Book());
+    return "/book/edit";
   }
 }
